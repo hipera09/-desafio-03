e79e11de741a844fdeb8206ecf78003039172530
Lucas Henrique Gonzaga de Sales <lucas7henrique@gmail.com>
2021-08-01 22:30:49 -0300

    Update Button.js

--- src/Button.js
+++ src/Button.js
@@ -12,8 +12,12 @@ export const Button = ({
     buttonStyle,
     buttonSize
 }) => {
+    const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
+
+    const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
     return (<Link to='/sign-up'>
         <button
+            className={'btn ${checkButtonStyle} ${checkButtonSize}'}
             onClick={onClick}
             type={type}
         >
